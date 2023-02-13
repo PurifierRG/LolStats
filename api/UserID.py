87b93f263bd8251e4423022d7b005f7d3b7b04c8
Prashant Jha <prashantjha1519@gmail.com>
2023-02-14 03:44:51 +0530

    "Fixed name bug"

--- api/UserID.py
+++ api/UserID.py
@@ -5,10 +5,11 @@ def getUser(api, username, region):
         url = f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}'
         response = requests.get(url, params={'api_key':api})
         resp = response.json()
+        name = resp['name']
         id = resp['id']
         accountId = resp['accountId']
         puuid = resp['puuid']
-        resp = {'id': id, 'accountId': accountId, 'puuid': puuid}
+        resp = {'id': id, 'accountId': accountId, 'puuid': puuid, 'name': name}
         return resp
     else:
         return "please fill the username"
